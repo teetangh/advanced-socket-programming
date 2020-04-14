4401ca21d11aefbbebb4fa1eb9d53b6737e83135
Kaustav Ghosh <teetangh@gmail.com>
2020-04-15 02:50:04 +0530

    added some includes

---
+++ includes/my_includes.h
@@ -0,0 +1,22 @@
+#ifndef MY_INCLUDES
+#define MY_INCLUDES
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <time.h>
+
+void print_me_test()
+{
+    printf("Print me test successful\n");
+}
+
+#endif
